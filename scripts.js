84468c66f44e2c9bff04278e1f6f27aec460e1e5
Michael Megrath <michaelmegrath25@gmail.com>
2019-06-12 07:15:46 -0700

    Modified html to pull content from scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const title = React.createElement(
+  'h1',
+  {id: 'main-title', title: 'This is a title'},
+  "Michael is trying his best."
+);
+
+ReactDOM.render(
+  title,
+  document.getElementById('root')
+);
